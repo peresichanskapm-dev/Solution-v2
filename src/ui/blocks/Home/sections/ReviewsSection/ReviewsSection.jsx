59b3f58996cf8f72ace061fc91cadefb2e2d8f05
vladyslad_dubinin <whatislove.dubinin@gmail.com>
2026-03-19 14:28:56 +0200

    fixes

--- src/ui/blocks/Home/sections/ReviewsSection/ReviewsSection.jsx
+++ src/ui/blocks/Home/sections/ReviewsSection/ReviewsSection.jsx
@@ -1,28 +1,16 @@
-import Image from "next/image";
+﻿import Image from "next/image";
 import classes from "./ReviewsSection.module.scss";
 
 export default function ReviewsSection() {
 	return (
 		<section className={classes.reviews}>
 			<div className={classes.reviewsBackground}>
-				<Image
-					src="/media/home/authorBackground.png"
-					alt="Forest"
-					fill
-					sizes="(max-width: 768px) 100vw, 96vw"
-					className={classes.reviewsBackgroundImage}
-				/>
+				<Image src="/media/home/reviewsBackgroundDesktop.png" alt="Forest" fill sizes="(max-width: 768px) 100vw, 1388px" className={classes.reviewsBackgroundImage} />
 			</div>
 			<div className={classes.reviewsOverlay} />
 
 			<div className={classes.reviewsCircle}>
-				<Image
-					src="/media/home/reviewsCirclePhone.png"
-					alt="Відгуки після SOLUTION"
-					fill
-					sizes="(max-width: 768px) 72vw, 48vw"
-					className={classes.reviewsCircleImage}
-				/>
+				<Image src="/media/home/reviewsCirclePhone.png" alt="Що відчувають після SOLUTION" fill sizes="(max-width: 768px) 370px, 695px" className={classes.reviewsCircleImage} />
 			</div>
 
 			<p className={classes.reviewsQuoteLeft}>«Наче хтось прибрав шум.»</p>
