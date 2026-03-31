81fd92f6e19a71a0a42ab396f48d35f2a6390dd6
vladyslad_dubinin <whatislove.dubinin@gmail.com>
2026-03-31 11:34:53 +0300

    Fixes

--- src/ui/blocks/Home/sections/StateIssueSection/StateIssueSection.jsx
+++ src/ui/blocks/Home/sections/StateIssueSection/StateIssueSection.jsx
@@ -16,12 +16,7 @@ export default function StateIssueSection() {
 			</div>
 
 			<div className={classes.issueDiagram} aria-hidden="true">
-				<Image src="/media/home/issueOuterText.png" alt="" width={900} height={925} className={`${classes.issueLayer} ${classes.issueOuterText}`} />
-				<Image src="/media/home/issueRingImage.png" alt="" width={800} height={800} className={`${classes.issueLayer} ${classes.issueRingImage}`} />
-				<Image src="/media/home/issueInnerText.png" alt="" width={646} height={682} className={`${classes.issueLayer} ${classes.issueInnerText}`} />
-				<Image src="/media/home/issueInnerStroke.png" alt="" width={512} height={512} className={`${classes.issueLayer} ${classes.issueInnerStroke}`} />
-				<Image src="/media/home/issueCenterFill.png" alt="" width={60} height={50} className={`${classes.issueLayer} ${classes.issueCenterFill}`} />
-				<Image src="/media/home/issueCenterStroke.png" alt="" width={301} height={373} className={`${classes.issueLayer} ${classes.issueCenterStroke}`} />
+				<Image src="/media/home/StateIssueBackground.png" alt="" width={561} height={561} className={classes.issueDiagramImage} />
 			</div>
 		</section>
 	);
